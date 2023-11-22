1b6e04138936bc3973cfd31a900e536a4a9d4169
Williams-Daniel <tito4bbt@gmail.com>
2023-11-22 04:32:29 +0100

    addToCart

--- router/productRouter.ts
+++ router/productRouter.ts
@@ -1,14 +1,16 @@
 import express, { Router } from "express"
-import { allProducts, createProduct, deleteProduct, oneProduct, updateProduct } from "../controller/productController"
+import { addProductToCart, allProducts, createProduct, deleteProduct, emptyCart, oneProduct, updateProduct } from "../controller/productController"
 import multer from "multer"
 
 const upload = multer().single("image")
  const router:Router = express.Router()
 
  router.route("/create-product").post(upload,createProduct)
+ router.route("/:userID/:productID/add-to-cart").post(addProductToCart)
  router.route("/:productID/update-product").patch(upload,updateProduct)
  router.route("/read-products").get(allProducts)
  router.route("/:productID/one-product").get(oneProduct)
  router.route("/:productID/delete-product").delete(deleteProduct)
+ router.route("/:userID/empty-cart").patch(emptyCart)
 
  export default router
